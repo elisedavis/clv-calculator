48b936b4383cd16df29b847bc180dc2bbb461a8e
elisedavis <133924008+elisedavis@users.noreply.github.com>
2023-05-17 17:59:09 -0700

    Delete forms.py

--- forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, IntegerField, SubmitField
-from wtforms.validators import DataRequired, Email, NumberRange, Length
-
-class clvData(FlaskForm): 
-    email = StringField('Email', validators=[DataRequired()])
-    apiKey = PasswordField('Private API Key', validators=[DataRequired(),Length(min=30, max=40)])
-    clvThreshold = IntegerField('CLV Group Size', validators=[NumberRange(min=1, max=100, message='Must be a number between 1 and 100')])
-    submit = SubmitField('Calculate')
