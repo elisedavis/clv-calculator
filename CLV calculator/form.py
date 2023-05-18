d9c830abd0e606f72978d857c9cfdd85adff3354
elisedavis <133924008+elisedavis@users.noreply.github.com>
2023-05-17 17:58:22 -0700

    Add files via upload

---
+++ CLV calculator/form.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, flash
+from forms import clvData
+from calc import calculate
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8277341ae6e7914ce05b29ca1e13a399'
+
+user_data = {
+    'user_percentile': '',
+    'api_key': '',
+    'email': ''  
+}
+api_response = ''
+
+@app.route("/", methods=['GET','POST'])
+def clvInputs():
+    form = clvData()
+    # if form.validate_on_submit():
+    #     apiKey = form.data.get('apiKey')
+    #     email = form.data.get('email')
+    #     percentile = form.data.get('clvThreshold')
+    #     flash("Success! We'll email you the results")
+    #     calculate(apiKey,email,percentile)
+    return render_template('Calculator.html', user_data=user_data, form=form)
+
+@app.route("/confirmation", methods=['GET','POST'])
+def clvOutputs():
+    form = clvData()
+    if form.validate_on_submit():
+        apiKey = form.data.get('apiKey')
+        email = form.data.get('email')
+        percentile = form.data.get('clvThreshold')
+        flash("Success! We'll email you the results")
+        calculate(apiKey,email,percentile)
+    return render_template('Calculator_results.html', user_data=user_data)
+
+
+if __name__ == '__main__': 
+    app.run(debug=True)
+
